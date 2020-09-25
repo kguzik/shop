17ecc864b177442bc7382d02be84ef7bb23bc3b8
kguzik <karolina.guzik08@gmail.com>
2020-09-25 18:57:46 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Products from './components/Products';
+import Home from './components/Home';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <main>
-        <Products/>
+        <Router>
+            <Route path="/" exact component={Home}/>
+            <Route path="/products" exact component={Products}/>
+        </Router>
       </main>
     </>
   );

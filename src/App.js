84fa77d8f813bdb57cfb71b65b9a62a6dbb6648c
kguzik <karolina.guzik08@gmail.com>
2020-09-26 23:31:45 +0200

    displaying component header in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Products from './components/Products';
 import Home from './components/Home';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+
+const shopName = 'online shop';
 
 function App() {
   return (
     <>
+    <Header shopName={shopName}/>
       <main>
         <Router>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact render={(props) => (<Home {...props} title="Welcome!" linkText="See products"/>)}/>
             <Route path="/products" exact component={Products}/>
         </Router>
       </main>

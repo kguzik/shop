a2d2bc130feabc4a7d82de3c20ff5f937966f077
kguzik <karolina.guzik08@gmail.com>
2020-09-26 21:03:43 +0200

    added filtering by size

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GetData from '../services/GetData';
 
 function Products() {
   const { data: products, error, loading } = GetData("products");
+  const [size, setSize] = useState(null);
+
+  const filteredProducts = size ? products.filter(product => product.size === parseInt(size)) : products;
 
   if(error) throw error;
   if(loading) return ( <h1>Loading..</h1>)  
 
   return (
     <>
-      <ul>
-        {products.map(product => <li key={product.id}>{product.name}</li>)}
-      </ul>
+      <section className="products">
+        <label htmlFor="size">Fitler by size</label>
+        <select id="size" className="products__filtr" value={size} onChange={(e) => setSize(e.target.value)}>
+          <option value="" selected>All sizes</option>
+          <option value="36">36</option>
+          <option value="38">38</option>
+        </select>
+        {filteredProducts.map(product => 
+          <div className="products__single" key={product.id}>
+            <img className="products__img" src={product.img} alt={product.img}/>
+            <p className="products__name">{product.name}</p>
+          </div>
+        )}
+      </section>
     </>
   );
 }

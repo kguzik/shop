de00372eac98719005e3def01b31f02c89df0401
kguzik <karolina.guzik08@gmail.com>
2020-09-26 23:29:21 +0200

    changes in the appearance of a component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
-function Home() {
+function Home(props) {
   return (
     <>
-      <h1>Welcome!</h1>
-      <a href="/products">See products</a>
+      <section className="home-page">
+        <Typography variant="h1" className="home-page__title">{props.title}</Typography>
+        <a href="/products" className="home-page__link">{props.linkText}</a>
+      </section>
     </>
   );
 }

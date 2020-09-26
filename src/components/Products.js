95c521651d4f9c6af3e4af176b6b2547c9e0154c
kguzik <karolina.guzik08@gmail.com>
2020-09-26 23:29:47 +0200

    changes in the appearance of a component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import GetData from '../services/GetData';
+import Grid from '@material-ui/core/Grid';
+import { LinearProgress } from '@material-ui/core';
 
 function Products() {
   const { data: products, error, loading } = GetData("products");
@@ -8,23 +10,35 @@ function Products() {
   const filteredProducts = size ? products.filter(product => product.size === parseInt(size)) : products;
 
   if(error) throw error;
-  if(loading) return ( <h1>Loading..</h1>)  
+  if(loading) return <LinearProgress />; 
 
   return (
     <>
       <section className="products">
-        <label htmlFor="size">Fitler by size</label>
-        <select id="size" className="products__filtr" value={size} onChange={(e) => setSize(e.target.value)}>
-          <option value="" selected>All sizes</option>
-          <option value="36">36</option>
-          <option value="38">38</option>
-        </select>
-        {filteredProducts.map(product => 
-          <div className="products__single" key={product.id}>
-            <img className="products__img" src={product.img} alt={product.img}/>
-            <p className="products__name">{product.name}</p>
-          </div>
-        )}
+        <Grid container>
+          <Grid item xs={12} md={2} justify="center" className="products__filters">
+            <aside>
+              <label htmlFor="size">Fitler by size</label>
+              <select id="size" className="products__filtr" value={size} onChange={(e) => setSize(e.target.value)}>
+                <option value="" selected>All sizes</option>
+                <option value="36">36</option>
+                <option value="38">38</option>
+              </select>
+            </aside>
+          </Grid>
+          <Grid item xs={12} md={10}>
+            <Grid container>
+            {filteredProducts.map(product => 
+              <Grid item xs={12} sm={6} md={4} key={product.id} justify="center" className="products__single">
+                <div>
+                  <img className="products__img" src={product.img} alt={product.img}/>
+                  <p className="products__name">{product.name}</p>
+                </div>
+              </Grid>
+            )}
+            </Grid>
+          </Grid>
+        </Grid>
       </section>
     </>
   );

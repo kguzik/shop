1427519c5b43c9085c24371c7f8e7893e6b3c56b
kguzik <karolina.guzik08@gmail.com>
2020-09-27 20:40:11 +0200

    component displaying information about the error page not found created

---
+++ src/components/errors/PageNotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function PageNotFound(props) {
+  return <Typography variant="h2" className="title">{props.title}</Typography>
+}
+
+export default PageNotFound;

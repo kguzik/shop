4e5315beb80ca8a8506bff3958dd08ebf3e2dbec
kguzik <karolina.guzik08@gmail.com>
2020-09-27 20:41:57 +0200

    component displaying home page added

---
+++ src/components/home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Category from './Category';
+import Grid from '@material-ui/core/Grid';
+
+function Home(props) {
+  return (
+    <>
+      <section className="home-page">
+        <Typography variant="h1" className="home-page__title">{props.title}</Typography>
+        <Typography variant="h2" className="subtitle">{props.subtitle}</Typography>
+      </section>
+      <section> 
+        <Grid container className="categories">
+          <Category title="Jackets" link="/jackets" linkName="See jackets" categoryClassName="categories__item--jackets"/>
+          <Category title="Coats" link="/coats" linkName="See coats" categoryClassName="categories__item--coats"/>
+        </Grid>
+      </section>
+    </>
+  );
+}
+
+
+export default Home;

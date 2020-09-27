71dfc93415ff7f2814bed4696aef526c02450822
kguzik <karolina.guzik08@gmail.com>
2020-09-27 21:39:08 +0200

    changed a tag to Link component from react router

--- src/components/home/Category.js
+++ src/components/home/Category.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 function Category(props) {
   return (
     <>
       <Grid className={`categories__item ${ props.categoryClassName }`} xs={12} md={5} item={true}>
         <Typography variant="h3" className="home-page__title">{props.title}</Typography>
-        <a href={props.link} className="link">{props.linkName}</a>
+        <Link to={props.link} className="link">{props.linkName}</Link>
       </Grid>
     </>
   )

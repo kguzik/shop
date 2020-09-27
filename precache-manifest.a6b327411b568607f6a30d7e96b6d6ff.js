75833d59cf7e8ab51c33ee7dde591d318c78e9a6
kguzik <karolina.guzik08@gmail.com>
2020-09-27 20:53:22 +0200

    Updates

---
+++ precache-manifest.a6b327411b568607f6a30d7e96b6d6ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c863a5d48ccaaeb3106f22ff05dcb7",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "f55db2e479db9c42dc8d",
+    "url": "/shop/static/css/main.7e33ccf3.chunk.css"
+  },
+  {
+    "revision": "3281b64a67d3c9c24da8",
+    "url": "/shop/static/js/2.7206ebdb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/shop/static/js/2.7206ebdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55db2e479db9c42dc8d",
+    "url": "/shop/static/js/main.ed891af2.chunk.js"
+  },
+  {
+    "revision": "aa1945b71dc1f6fb53f2",
+    "url": "/shop/static/js/runtime-main.4a8e269c.js"
+  },
+  {
+    "revision": "78101c8dce3f19d163b8c6b251645f46",
+    "url": "/shop/static/media/background.78101c8d.jpeg"
+  },
+  {
+    "revision": "3b964dfa7383ea3fed80afedd80b1f2b",
+    "url": "/shop/static/media/coats.3b964dfa.jpeg"
+  },
+  {
+    "revision": "fcde16742587607e818a8821932eab83",
+    "url": "/shop/static/media/jackets.fcde1674.jpeg"
+  }
+]);

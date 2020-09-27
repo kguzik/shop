66ff0dc49f5fc853b234fe1efb1b1af4e9925a6d
kguzik <karolina.guzik08@gmail.com>
2020-09-27 20:38:39 +0200

    displaying menu in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,21 @@ function Header(props){
   return (
     <>
       <header className="header">
-        <a href="/" className="header__shop-name">{props.shopName}</a>
+        <nav>
+          <ul className="header__menu--left">
+            <li>
+              <a href="/" className="header__shop-name">{props.shopName}</a>
+            </li>
+          </ul>
+          <ul className="header__menu--right">
+            <li>
+              <a href="/jackets" className="header__links">jackets</a>
+            </li>
+            <li>
+              <a href="/coats" className="header__links">coats</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   )

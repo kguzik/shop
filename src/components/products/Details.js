6778c31ffeb1446b9d2bffdaaad7a4104b3b59ba
kguzik <karolina.guzik08@gmail.com>
2020-09-28 23:16:09 +0200

    selecting size and add to cart button added

--- src/components/products/Details.js
+++ src/components/products/Details.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import GetData from '../../services/GetData';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { LinearProgress } from '@material-ui/core';
 import PageNotFound from '../errors/PageNotFound';
+import Button from '@material-ui/core/Button';
 
-function Details() {
+function Details(props) {
   const { category, id } = useParams();
+  const history = useHistory();
   const { data: products, error, loading } = GetData(category);
+  const [sku, setSku] = useState(null);
 
   if(error) throw error;
   if(loading) return <LinearProgress />; 
 
   const productDetail = products.filter(product => product.id === parseInt(id))[0];
 
-  if(productDetail === undefined) return <PageNotFound title="Product not found"/>;
+  if(!productDetail) return <PageNotFound title="Product not found"/>;
 
   return (
     <>
@@ -28,8 +31,20 @@ function Details() {
             <Typography variant="h1" className="product__details--title">{productDetail.name}</Typography>
             <p className="product__details--description">{productDetail.description}</p>
             <p className="product__details--price">Price: {productDetail.price} &euro;</p>
-            <p className="product__details--size">Size: {productDetail.size}</p>
-            <p className="product__details--availability">In stock: {productDetail.availability}</p>
+            <select id="size" className="product__details--size" value={sku} onChange={(e) => setSku(e.target.value)}>
+              <option value="" defaultValue>Select size</option>
+              {productDetail.sizes.map(productSizes => <option value={productSizes.sku} key={productSizes.sku}>{productSizes.size} </option>)}
+            </select>
+            <Button 
+              disabled={!sku} 
+              className="btn" 
+              onClick={() => {
+                props.addToCart(category, id, sku);
+                history.push('/shopping-cart');
+              }}
+            >
+              Add to cart
+            </Button>
           </div>
         </Grid>
       </Grid>

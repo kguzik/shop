4fcc0d7a22f2811168e5327967e36740bbc2d35d
kguzik <karolina.guzik08@gmail.com>
2020-09-28 23:17:06 +0200

    link to product details changed

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -11,8 +11,7 @@ function Products() {
   const { data: products, error, loading } = GetData(category);
   const [size, setSize] = useState(null);
 
-  const filteredProducts = size ? products.filter(product => product.size === parseInt(size)) : products;
-
+  const filteredProducts = size ? products.filter((product) => product.sizes.find((productSize) => productSize.size === parseInt(size))) : products;
   if(error) throw error;
   if(loading) return <LinearProgress />; 
   if(products.length === 0) return <PageNotFound title="Products not found"/>;
@@ -21,11 +20,11 @@ function Products() {
     <>
       <section className="products">
         <Grid container>
-          <Grid item xs={12} md={2} justify="center" className="products__filters">
+          <Grid item xs={12} md={2} className="products__filters">
             <aside>
               <label htmlFor="size">Fitler by size</label>
               <select id="size" className="products__filtr" value={size} onChange={(e) => setSize(e.target.value)}>
-                <option value="" selected>All sizes</option>
+                <option value="" defaultValue>All sizes</option>
                 <option value="36">36</option>
                 <option value="38">38</option>
               </select>
@@ -34,14 +33,14 @@ function Products() {
           <Grid item xs={12} md={10}>
             <Grid container>
             {filteredProducts.map(product => 
-              <Grid item xs={12} sm={6} md={4} key={product.id} justify="center" className="product__single">
+              <Grid item xs={12} sm={6} md={4} key={product.id} className="product__single">
                 <div>
                   <img className="product__single--img" src={product.img} alt={product.img}/>
                   <p className="product__single--info">
                     <span className="product__single--name">{product.name}</span>
                     <span className="product__single--price">{product.price} &euro;</span>
                   </p>
-                  <Link className="link" to={`/${category}/${product.id}`}>See details</Link>
+                  <Link className="link" to={`/products/${category}/${product.id}`}>See details</Link>
                 </div>
               </Grid>
             )}

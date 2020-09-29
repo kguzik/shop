c3df9b563b9393c79b55bb4740980c8e25b3c9bc
kguzik <karolina.guzik08@gmail.com>
2020-09-29 17:25:03 +0200

    displaying items in shopping cart added

--- src/components/shop/ShoppingCart.js
+++ src/components/shop/ShoppingCart.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import GetAllData from '../../services/GetAllData';
 import { LinearProgress } from '@material-ui/core';
-import PageNotFound from '../errors/PageNotFound';
+//import PageNotFound from '../errors/PageNotFound';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-function ShoppingCart(props) {
+function ShoppingCart({ cart, updateQuantity }) {
   const {data: products, loading, error} = GetAllData(['jackets', 'coats']);
-  console.log(products);
+
+  function renderItem(itemInCart) {
+    const { id, sku, quantity } = itemInCart;
+    const { price, name, sizes, img } = products[0].concat(products[1]).find(
+      (p) => p.id === parseInt(id)
+    );
+    const { size } = sizes.find((s) => s.sku === parseInt(sku));
+    //const { availability } = sizes.find((s) => s.availability);
+   
+    return (
+      <Grid container key={sku} className="cart-item">
+        <Grid item xs={12} md={4} lg={2}>
+          <img src={img} alt={name} className="cart-item__img"/>
+        </Grid>
+        <Grid item xs={12} md={4} lg={2}>
+          <Typography variant="h2" className="cart-item__name">{name}</Typography>
+        </Grid>
+        <Grid item xs={12} md={4} lg={2}>
+          <p className="cart-item__price">{price} &euro;</p>
+        </Grid>
+        <Grid item xs={12} md={4} lg={2}>
+          <p className="cart-item__size">Size: {size}</p>
+        </Grid>
+        <Grid item xs={12} md={4} lg={2}>
+          <p className="cart-item__quantity">Quantity: {quantity}</p>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  if(error) throw error;
+  if(loading) return <LinearProgress />; 
+
   return (
     <>
-      <p>Shopping Cart</p>
+      <Typography variant="h1" textAlign="center">Shopping Cart</Typography>
+      <ul>{cart.map(renderItem)}</ul>
     </>
   )
 }

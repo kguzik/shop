36b38d2674ae053e282ce60fc0e5a21852f525a1
kguzik <karolina.guzik08@gmail.com>
2020-09-30 14:42:59 +0200

    number of items in cart displayed, ability to change the number and delete items added, custom styling for h1 added

--- src/components/shop/ShoppingCart.js
+++ src/components/shop/ShoppingCart.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import GetAllData from '../../services/GetAllData';
 import { LinearProgress } from '@material-ui/core';
-//import PageNotFound from '../errors/PageNotFound';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import _ from 'lodash';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-function ShoppingCart({ cart, updateQuantity }) {
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontSize: 40,
+      fontWeight: 400
+    }
+  },
+});
+
+function ShoppingCart({ cart, updateQuantity, removeItem }) {
   const {data: products, loading, error} = GetAllData(['jackets', 'coats']);
 
   function renderItem(itemInCart) {
@@ -14,8 +24,8 @@ function ShoppingCart({ cart, updateQuantity }) {
       (p) => p.id === parseInt(id)
     );
     const { size } = sizes.find((s) => s.sku === parseInt(sku));
-    //const { availability } = sizes.find((s) => s.availability);
-   
+    const { availability } = sizes.find((s) => s.availability);
+    
     return (
       <Grid container key={sku} className="cart-item">
         <Grid item xs={12} md={4} lg={2}>
@@ -30,19 +40,39 @@ function ShoppingCart({ cart, updateQuantity }) {
         <Grid item xs={12} md={4} lg={2}>
           <p className="cart-item__size">Size: {size}</p>
         </Grid>
-        <Grid item xs={12} md={4} lg={2}>
-          <p className="cart-item__quantity">Quantity: {quantity}</p>
+        <Grid item xs={12} md={4} lg={2} className="cart-item__actions">
+          <select className="cart-item__actions--change-quantity" value={quantity} onChange={(e) => updateQuantity(sku, parseInt(e.target.value))}>
+            { _.range(1, parseInt(availability) + 1, +1).map(value => <option key={value} value={value}>{value}</option>)}
+          </select>
+          <button 
+            className="cart-item__actions--remove"
+            onClick={() => {
+              removeItem(sku)
+            }}
+          >
+            &#10005;
+          </button>
         </Grid>
       </Grid>
+  
     );
   }
+
+  const numOfItems = cart.reduce(( allItems, item ) => allItems + item.quantity, 0);
 
   if(error) throw error;
   if(loading) return <LinearProgress />; 
 
   return (
     <>
-      <Typography variant="h1" textAlign="center">Shopping Cart</Typography>
+      <MuiThemeProvider theme={theme}>
+        <Typography variant="h1" className="cart__title">Shopping Cart</Typography>
+          <p className="cart__item-quantity">
+            { numOfItems > 0 
+              ? `You have ${numOfItems} item${numOfItems > 1 ? 's' : ''} in your cart`
+              : "Your cart is empty" }
+          </p>
+      </MuiThemeProvider>
       <ul>{cart.map(renderItem)}</ul>
     </>
   )
